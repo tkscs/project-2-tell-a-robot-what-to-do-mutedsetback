2b7cf13373e831b60092d16cf9d661e5e4a489bc
mutedsetback <bcrawford2028@kehillahstudent.org>
2026-01-08 08:49:28 -0800

    Main

--- main.py
+++ main.py
@@ -5,36 +5,65 @@ from simulator import robot, FORWARD, BACKWARD, STOP
 import time
 
 
+turn_direction = input("Turn left or right when obstacle detected (left/right)? ")
+x = int(input("Set the sonar sensitivity (recomended minimum 30): "))
+y = int(input("Set the maximum number of obstacles before the robot gets tired of you "))
+obstacle_count = 0
 
+def user_wants_to_quit():
+    """ Check if the user wants to quit based on the user input."""
+    if user_input == "q":
+        return True
+    else:
+        return False
 def forward():
-    """ Move the robot forward for a short duration."""
-    robot.motors(left=FORWARD, right=FORWARD, seconds=0.1)
+    """ Move the robot forward for a short duration. or until uther commanded is given"""
+    robot.motors(left=FORWARD, right=FORWARD, seconds=0.5)
 def stop():
-    """ Stop the robot for a short duration."""
+    """ Stop the robot for a specified duration."""
     robot.motors(left=STOP, right=STOP, seconds=0.05)
-def turnright(duration):
-    """ Turn the robot right for a specified duration."""
-    robot.motors(left=FORWARD, right=BACKWARD, seconds = duration)
-    stop()
+def turn(duration):
+    """ Turn the robot a direction specified by user input for a specified duration."""
+    if turn_direction == "right":
+        robot.motors(left=BACKWARD, right=FORWARD, seconds = duration)
+    else:   
+        robot.motors(left=FORWARD, right=BACKWARD, seconds = duration)
+        stop()
 
 start_time = time.time()
+last_asked_time = start_time
 
 while True:
     left = robot.left_sonar()
     right = robot.right_sonar()
     
 
-    if left < 40 or right < 40:
+    if left < x or right < x:
         stop()
-        time.sleep(0.1)
-        turnright(0.15)
+        turn(0.5)
     else:
         forward()
 
-    time.sleep(0.1)
+    if left < x or right < x:
+        obstacle_count += 1
+        print("Obstacles detected:", obstacle_count)
+    if obstacle_count >= y:
+        print("This space is too Complicated for me!!!!!")
+        robot.exit()
     
-    if time.time() - start_time > 150:
-        break
-robot.exit()
+    if time.time() - last_asked_time > 120:
+        user_input = input("Continue (c) or Quit (q)? ")
+        last_asked_time = time.time()
+        def user_wants_to_quit():
+            if user_input == "q":
+                return True
+            elif user_input == "c":
+                return False
+            else:
+                print("Invalid input, please enter 'c' to continue or 'q' to quit.")
+        if user_wants_to_quit():
+            robot.exit()
+            
+
 
 
